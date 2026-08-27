6df54d4f6bda4b9dc2bb5d8eb6a6a80c70cd90fc
CrackersLad <Olly_cool2003@hotmail.com>
2026-08-27 09:27:20 +0100

    chore(release): v0.27
    Convert lobby players roster into horizontal carousel and refine homescreen elements

--- public/profile.js
+++ public/profile.js
@@ -1,4 +1,4 @@
-import { db, auth } from './firebase-setup.js?v=0.26';
+import { db, auth } from './firebase-setup.js?v=0.27';
 import { ref, get, query, orderByChild, limitToLast } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 export function initProfileModule(utils, state) {
