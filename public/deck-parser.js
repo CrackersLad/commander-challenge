b6cade8949aa7750088f345b97c5e6c36efdbeea
CrackersLad <Olly_cool2003@hotmail.com>
2026-09-03 23:22:31 +0100

    chore(release): v4.13
    Preserve lockfile and add npm fetch retries to survive npm registry degradation

--- public/deck-parser.js
+++ public/deck-parser.js
@@ -1,4 +1,4 @@
-import { functions } from './firebase-setup.js?v=4.12';
+import { functions } from './firebase-setup.js?v=4.13';
 import { httpsCallable } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-functions.js";
 
 // Official Game Changers for Moxfield fallback (excluding casual staples like Sol Ring)
