7dcdfb1d949a08cd8c98d29f7d96165459b08c60
CrackersLad <Olly_cool2003@hotmail.com>
2026-09-07 15:48:23 +0100

    chore(release): v4.35
    Rename Archidekt Hub to Collection Hub and expand desktop layout width

--- public/deck-actions.js
+++ public/deck-actions.js
@@ -1,5 +1,5 @@
-import { db } from './firebase-setup.js?v=4.32';
-import { fetchDeckPriceLocal } from './deck-parser.js?v=4.32';
+import { db } from './firebase-setup.js?v=4.35';
+import { fetchDeckPriceLocal } from './deck-parser.js?v=4.35';
 import { ref, get, update } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 export function initDeckActionsModule(utils, state) {
