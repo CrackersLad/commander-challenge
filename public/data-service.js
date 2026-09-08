cf1a5b379949a055f117f8380a2c9242f2273266
CrackersLad <Olly_cool2003@hotmail.com>
2026-09-08 22:51:09 +0100

    chore(release): v6.12
    Add real-time Archidekt commander search, fix TypeError in restoreCachedComparison, and clarify Exclude Searched Decks in Deck Comparator

--- public/data-service.js
+++ public/data-service.js
@@ -1,4 +1,4 @@
-import { db } from './firebase-setup.js?v=6.10';
+import { db } from './firebase-setup.js?v=6.12';
 import { ref, get } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 let localArchives = null;
--- public/deck-actions.js
+++ public/deck-actions.js
@@ -1,5 +1,5 @@
-import { db } from './firebase-setup.js?v=6.10';
-import { fetchDeckPriceLocal } from './deck-parser.js?v=6.10';
+import { db } from './firebase-setup.js?v=6.12';
+import { fetchDeckPriceLocal } from './deck-parser.js?v=6.12';
 import { ref, get, update } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 export function initDeckActionsModule(utils, state) {
