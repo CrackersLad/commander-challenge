868c958a4d2440da73c2b64d0ab5de15d55a340e
CrackersLad <Olly_cool2003@hotmail.com>
2026-09-17 23:09:36 +0100

    chore(release): v7.2
    Reset and wipe session container on disconnect and auto-scale playtest window

--- public/deck-actions.js
+++ public/deck-actions.js
@@ -1,5 +1,5 @@
-import { db } from './firebase-setup.js?v=7.1';
-import { fetchDeckPriceLocal, fetchDeckFromAPI } from './deck-parser.js?v=7.1';
+import { db } from './firebase-setup.js?v=7.2';
+import { fetchDeckPriceLocal, fetchDeckFromAPI } from './deck-parser.js?v=7.2';
 import { ref, get, update } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 export function initDeckActionsModule(utils, state) {
