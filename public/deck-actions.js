8c5ed2ae01ae1c72810d01c8d85a5b191ce89fc1
CrackersLad <Olly_cool2003@hotmail.com>
2026-09-17 11:57:24 +0100

    chore(release): v7.1
    Fix dashboard card and commander image sizing leak in collection-hub.css

--- public/deck-actions.js
+++ public/deck-actions.js
@@ -1,5 +1,5 @@
-import { db } from './firebase-setup.js?v=7.0';
-import { fetchDeckPriceLocal, fetchDeckFromAPI } from './deck-parser.js?v=7.0';
+import { db } from './firebase-setup.js?v=7.1';
+import { fetchDeckPriceLocal, fetchDeckFromAPI } from './deck-parser.js?v=7.1';
 import { ref, get, update } from "https://www.gstatic.com/firebasejs/9.22.0/firebase-database.js";
 
 export function initDeckActionsModule(utils, state) {
